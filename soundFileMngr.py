1556ae529c7f8903b55032b3328f6dce02ec8d80
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-13 00:24:46 -0600

    Adds getToyExample for testing

--- soundFileMngr.py
+++ soundFileMngr.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import os, os.path
+import cPickle
+import librosa
 
 def milliseconds2samples(numMilli, samplerate):
     return int(numMilli * float(samplerate) / 1000)
@@ -32,3 +35,29 @@ def getSegmentsByTime(musicArray, samplerate, segmentLengthMilli):
     windowSize = milliseconds2samples(segmentLengthMilli, samplerate)
     return getSegments(musicArray, windowSize)
     
+
+def loadAndPickleFiles():
+    librosa_loader = librosa.load
+
+    directory_names = ['Screams','Bangs and other manmade sounds','Singing','Birds']
+    screams,singing,birds,bangs = [],[],[],[]
+    my_sounds = {"Screams":screams,"Singing":singing,"Birds":birds,"Bangs and other manmade sounds":bangs}
+    
+    for folder in directory_names:
+        for sound_file in os.listdir("small_set/"+folder):
+            print("folder: ",folder," sound_file: ",sound_file)
+            sound,sr = librosa_loader(folder+'/'+sound_file,sr=44100)
+            my_sounds[folder].append((sound,sr))
+
+    with open('small_set_sounds.pkl','wb') as outfile:
+        cPickle.dump(my_sounds,outfile)
+    return my_sounds
+
+def getToySounds():
+    try:
+        with open('small_set_sounds.pkl','rb') as infile:
+            my_sounds = cPickle.load(infile)
+    except:
+        my_sounds = loadAndPickleFiles()
+    
+    return my_sounds
