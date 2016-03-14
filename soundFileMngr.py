d5bb8239c551cb9e330df06dacfb17ba5257ab51
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-14 18:08:33 -0500

    refactors sound management out of file management

--- soundFileMngr.py
+++ soundFileMngr.py
@@ -1,40 +1,7 @@
 # -*- coding: utf-8 -*-
-import numpy as np
 import os, os.path
 import cPickle
 import librosa
-
-def milliseconds2samples(numMilli, samplerate):
-    return int(numMilli * float(samplerate) / 1000)
-
-def getSegments(musicArray, windowSize):
-    if windowSize % 2:
-        raise ArithmeticError('windowSize cannot be odd')
-
-    musicLength = len(musicArray)
-    hopSize = windowSize/2
-    
-    # figure out how many hops
-    length_to_cover_with_hops = musicLength - windowSize;
-    
-    if length_to_cover_with_hops < 0:
-        raise ArithmeticError("window_size cannot be longer than the signal to be windowed")
-        
-    num_hops = 1 + length_to_cover_with_hops/hopSize;    
-    segments = [0]*num_hops
-    # fill the array with values 
-    for hop in range(num_hops):
-        start = hop*hopSize
-        end = start + windowSize
-        if end > musicLength:
-            end = musicLength
-        segments[hop] = musicArray[start:end]
-    return np.array(segments)
-    
-def getSegmentsByTime(musicArray, samplerate, segmentLengthMilli):
-    windowSize = milliseconds2samples(segmentLengthMilli, samplerate)
-    return getSegments(musicArray, windowSize)
-    
 
 def loadAndPickleFiles():
     librosa_loader = librosa.load
