cf1973a1cc0ac7f80b8bf26149be72cc4a8e3014
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-15 01:00:40 -0500

    Creates rudimentary tree and prints

--- machineLearning.py
+++ machineLearning.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
+from sklearn.tree import export_graphviz as eGraph, DecisionTreeClassifier as dTreeClassifier
+import machineLearningHelper as mlh
+import numpy as np
+
+instances, classifications = mlh.getLearningArrays(useToySounds=True)
 
 
+
+instancesFeatures = mlh.getInstancesFeatures(instances)
+classifications = np.array(classifications)
+
+print "Instance features type ", instancesFeatures.dtype
+print "classifications type ", classifications.dtype
+
+dTree = dTreeClassifier()
+dTree = dTree.fit(instancesFeatures, classifications)
+eGraph(dTree, out_file='dTree.dot')
