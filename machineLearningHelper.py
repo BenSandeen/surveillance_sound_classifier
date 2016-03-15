6e5e0bd91e3687a67a4627d05e8944a5f0bdc7e6
Juan David <juandominguez2017@u.northwestern.edu>
2016-03-15 01:00:18 -0500

    So far, all helper functions for machine learnging :

--- machineLearningHelper.py
+++ machineLearningHelper.py
@@ -1,9 +1,56 @@
 # -*- coding: utf-8 -*-
 import soundMngr as sm
 import soundFileMngr as sfm
+import librosa
+import numpy as np
 
 timeForSubSegment = 40 #in milliseconds
 samplerate = 44100
+segmentSize = samplerate * timeForSubSegment / 1000
+
+def make_extender(n):
+    return lambda x: n.extend(x.flatten())
+
+def featurify(soundSegment):
+    # should be vector of dimensions 20x1
+    sgram = librosa.feature.melspectrogram(y=soundSegment,sr=samplerate,n_fft=segmentSize,
+                                           hop_length=segmentSize)
+    mfcc = librosa.feature.mfcc(S=librosa.logamplitude(sgram), sr=samplerate)
+    
+    # hop_length and window_size are the width of the segment
+    pre_attack = librosa.feature.rmse(y=soundSegment,n_fft=segmentSize,hop_length=segmentSize)
+    pre_attack = pre_attack.flatten() # may need to return the element inside this: eg: [[a]]
+
+    # look at median of:
+        # spectral_centroid = librosa.feature.delta(spectral_centroid)
+        # spectral_centroid = np.square(spectral_centroid)
+    spectral_centroid = librosa.feature.spectral_centroid(y=soundSegment,
+                                                          sr=samplerate,
+                                                          n_fft=segmentSize,
+                                                          hop_length=segmentSize)
+    
+    chroma = librosa.feature.chroma_stft(y=soundSegment, sr=samplerate,
+                                            n_fft=segmentSize,hop_length=segmentSize)
+    chroma_std = np.std(chroma,axis=0) # may not need axis=0 arg
+    chroma_avg = np.mean(chroma,axis=0)    
+    
+    attack = librosa.feature.delta(soundSegment)#,order=2)
+    features = []
+    extend_features = make_extender(features)
+    extend_features(mfcc)
+    extend_features(pre_attack)
+    extend_features(spectral_centroid)
+    extend_features(chroma)
+    extend_features(chroma_avg)
+    extend_features(chroma_std)
+    return np.array(features, dtype=np.float32)
+
+def getInstancesFeatures(instances):
+    instancesFeatures = []
+    for instance in instances:
+        features = featurify(instance)
+        instancesFeatures.append(features)
+    return np.array(instancesFeatures, dtype=np.float32)
 
 def getSoundsFromTupleList(sound_sr_tuple_list):
     return list(zip(*sound_sr_tuple_list)[0])
